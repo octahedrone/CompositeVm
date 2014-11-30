ecee13035fd09c6a097f16e44aeb16926a069081
octahedrone <octahedrone@gmail.com>
2014-11-30 18:43:15 +0200

    Empty project with configuration presets

---
+++ src/CommonAssemblyInfo.cs
@@ -0,0 +1,10 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyCompany("Octahedrone")]
+[assembly: AssemblyProduct("Composite VM")]
+[assembly: ComVisible(false)]
+
+[assembly: AssemblyConfiguration("Release")]
+[assembly: AssemblyVersion("0.0.0.0")]
+[assembly: AssemblyFileVersion("0.0.0.0")]
---
+++ src/Composite.Core/Properties/AssemblyInfo.cs
@@ -0,0 +1,5 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Composite.Core")]
+[assembly: Guid("0f40d542-4313-4e8f-94e5-03d0dec73f26")]
