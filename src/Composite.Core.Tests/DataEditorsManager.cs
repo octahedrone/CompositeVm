553fab421ac0c4def154d9db1130d958d0003144
octahedrone <octahedrone@gmail.com>
2015-04-18 12:34:49 +0300

    Extracting editors synchronization logic to the DataEditorsManager

---
+++ src/Composite.Core.Tests/DataEditorsManager.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace Composite.Core.Tests
+{
+    public class DataEditorsManager<TData> : IEnumerable<IDataEditor<TData>>
+    {
+        private readonly LinkedList<IDataEditor<TData>> _editors = new LinkedList<IDataEditor<TData>>();
+        private TData _editableTarget;
+
+        public void Add(IDataEditor<TData> editor)
+        {
+            if (_editors.Contains(editor))
+            {
+                return;
+            }
+
+            _editors.AddLast(editor);
+
+            editor.TargetUpdated += OnEditorUpdatesTarget;
+            
+            editor.EditableTarget = _editableTarget;
+        }
+
+        public void Remove(IDataEditor<TData> editor)
+        {
+            editor.TargetUpdated -= OnEditorUpdatesTarget;
+            
+            _editors.Remove(editor);
+        }
+
+        public TData EditableTarget
+        {
+            get { return _editableTarget; }
+            set
+            {
+                _editableTarget = value;
+
+                foreach (var editor in _editors)
+                {
+                    editor.EditableTarget = _editableTarget;
+                }
+            }
+        }
+
+        public IEnumerator<IDataEditor<TData>> GetEnumerator()
+        {
+            return _editors.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        private void OnEditorUpdatesTarget(object sender, PropertyUpdatedEventArgs e)
+        {
+            var updater = sender as IDataEditor<TData>;
+
+            if (updater == null)
+            {
+                return;
+            }
+
+            _editableTarget = updater.EditableTarget;
+
+            foreach (var editor in _editors)
+            {
+                if (editor != sender)
+                {
+                    editor.EditableTarget = _editableTarget;
+                }
+            }
+        }
+    }
+}
---
+++ src/Composite.Core.Tests/DataEditorsManagerTests.cs
@@ -0,0 +1,126 @@
+﻿using System.Collections.Generic;
+using Composite.Core.Tests.EditrableTargets;
+using FluentAssertions;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace Composite.Core.Tests
+{
+    [TestFixture]
+    public class DataEditorsManagerTests
+    {
+        private DataEditorsManager<EditableStruct> _sut;
+        private readonly IEqualityComparer<EditableStruct> _comparer = new TextEqualityComparer();
+
+        [Test]
+        public void EditorsTargetIsInitializedAtInsertion()
+        {
+            var target = new EditableStruct {Text = "Initial"};
+
+            var editorA = Substitute.For<IDataEditor<EditableStruct>>();
+            var editorB = Substitute.For<IDataEditor<EditableStruct>>();
+
+            // given
+            _comparer.Equals(editorA.EditableTarget, target).Should().BeFalse();
+            _comparer.Equals(editorB.EditableTarget, target).Should().BeFalse();
+
+            // act
+            _sut = new DataEditorsManager<EditableStruct>
+            {
+                EditableTarget = target
+            };
+
+            _sut.Add(editorA);
+            _sut.Add(editorB);
+
+            // assert
+            _comparer.Equals(editorA.EditableTarget, target).Should().BeTrue();
+            _comparer.Equals(editorB.EditableTarget, target).Should().BeTrue();
+        }
+
+        [Test]
+        public void EditorsTargetIsUpdatedWhenManagersIsUpdated()
+        {
+            var target = new EditableStruct {Text = "Initial"};
+
+            var editorA = Substitute.For<IDataEditor<EditableStruct>>();
+            var editorB = Substitute.For<IDataEditor<EditableStruct>>();
+
+            _sut = new DataEditorsManager<EditableStruct>
+            {
+                editorA,
+                editorB
+            };
+
+            // given
+            _comparer.Equals(editorA.EditableTarget, target).Should().BeFalse();
+            _comparer.Equals(editorB.EditableTarget, target).Should().BeFalse();
+
+            // act
+            _sut.EditableTarget = target;
+
+            // assert
+            _comparer.Equals(editorA.EditableTarget, target).Should().BeTrue();
+            _comparer.Equals(editorB.EditableTarget, target).Should().BeTrue();
+        }
+
+        [Test]
+        public void AllEditorsTargetsAreSynchronizedOnSingleEditorUpdate()
+        {
+            var target = new EditableStruct {Text = "Initial"};
+
+            var editorA = Substitute.For<IDataEditor<EditableStruct>>();
+            var editorB = Substitute.For<IDataEditor<EditableStruct>>();
+
+            _sut = new DataEditorsManager<EditableStruct>
+            {
+                editorA,
+                editorB
+            };
+
+            _sut.EditableTarget = target;
+
+            // act
+            var updatedTarget = new EditableStruct { Text = "Updated" };
+            editorB.EditableTarget = updatedTarget;
+
+            var args = new PropertyUpdatedEventArgs("Text");
+            editorB.TargetUpdated += Raise.EventWith(args);
+
+            // assert
+            _comparer.Equals(editorA.EditableTarget, updatedTarget).Should().BeTrue();
+            _comparer.Equals(_sut.EditableTarget, updatedTarget).Should().BeTrue();
+        }
+
+        [Test]
+        public void RemovedEditorIsManagedNoMore()
+        {
+            var target = new EditableStruct {Text = "Initial"};
+
+            var editorA = Substitute.For<IDataEditor<EditableStruct>>();
+            var editorB = Substitute.For<IDataEditor<EditableStruct>>();
+
+            _sut = new DataEditorsManager<EditableStruct>
+            {
+                editorA,
+                editorB
+            };
+
+            _sut.EditableTarget = target;
+
+            // act
+            var updatedTarget = new EditableStruct { Text = "Updated" };
+
+            _sut.Remove(editorB);
+
+            _sut.EditableTarget = updatedTarget;
+
+            var args = new PropertyUpdatedEventArgs("Text");
+            editorB.TargetUpdated += Raise.EventWith(args);
+
+            // assert
+            _comparer.Equals(editorB.EditableTarget, target).Should().BeTrue();
+            _comparer.Equals(_sut.EditableTarget, updatedTarget).Should().BeTrue();
+        }
+    }
+}
