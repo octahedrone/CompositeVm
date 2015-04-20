d0e9a974734d94aa9a11ee29bfc97c7cb981c643
octahedrone <octahedrone@gmail.com>
2015-04-20 12:36:54 +0300

    Adding dumb PropertyChangedBase implementation

---
+++ src/Composite.Core/PropertyChangedBase.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel;
+
+namespace Composite.Core
+{
+    public class PropertyChangedBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            var handler = PropertyChanged;
+
+            if (handler != null)
+                handler(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
