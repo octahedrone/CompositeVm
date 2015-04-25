78fe296eaf6999be89ca5d1bca6a43aa3d6b3ddc
octahedrone <octahedrone@gmail.com>
2015-04-25 22:31:36 +0300

    Implementing IPropertyAdapter by ClassTypeGenericPropertyAdapter.

---
+++ src/Composite.Core.Tests/ClassTypeGenericPropertyAdapterTests.cs
@@ -0,0 +1,51 @@
+﻿using Composite.Core.PropertyAdapaters;
+using Composite.Core.Tests.ClassTargetTests.Target;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace Composite.Core.Tests
+{
+    [TestFixture]
+    public class ClassTypeGenericPropertyAdapterTests
+    {
+        [Test]
+        public void PropertyNameIsRetrievedCorrectly()
+        {
+            var sut = new ClassTypeGenericPropertyAdapter<EditableClass, string>(_ => _.Text);
+
+            // assert
+            sut.PropertyName.Should().Be("Text");
+            sut.IsReadOnly.Should().BeTrue();
+        }
+
+        [Test]
+        public void PropertyValueIsRetrievedCorrectly()
+        {
+            var sut = new ClassTypeGenericPropertyAdapter<EditableClass, string>(_ => _.Text);
+
+            // act
+            const string text = "Value";
+            var value = sut.GetValue(new EditableClass {Text = text});
+
+            // assert
+            value.Should().Be(text);
+        }
+
+        [Test]
+        public void PropertyValueIsUpdatedCorrectly()
+        {
+            var sut = new ClassTypeGenericPropertyAdapter<EditableClass, string>(_ => _.Text, (t, v) => t.Text = v);
+
+            const string initialText = "Initial";
+            const string updatedText = "Updated";
+
+            // act
+            var target = new EditableClass {Text = initialText};
+            sut.SetValue(target, updatedText);
+
+            // assert
+            sut.IsReadOnly.Should().BeFalse();
+            target.Text.Should().Be(updatedText);
+        }
+    }
+}
---
+++ src/Composite.Core/PropertyAdapaters/ClassTypeGenericPropertyAdapter.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace Composite.Core.PropertyAdapaters
+{
+    public class ClassTypeGenericPropertyAdapter<TEntity, TValue> : IPropertyAdapter<TEntity, TValue>
+        where TEntity : class
+    {
+        private readonly string _propertyName;
+        private readonly Action<TEntity, TValue> _valueSetter;
+        private readonly Func<TEntity, TValue> _valueGetter;
+
+        public ClassTypeGenericPropertyAdapter(Expression<Func<TEntity, TValue>> getterExpression) 
+            : this(getterExpression, null)
+        {
+        }
+
+        public ClassTypeGenericPropertyAdapter(Expression<Func<TEntity, TValue>> getterExpression, Action<TEntity, TValue> valueSetter)
+        {
+            _valueGetter = getterExpression.Compile();
+            _valueSetter = valueSetter;
+
+            _propertyName = GetPropertyName(getterExpression);
+        }
+
+        public string PropertyName
+        {
+            get { return _propertyName; }
+        }
+
+        public bool IsReadOnly
+        {
+            get { return _valueSetter == null; }
+        }
+
+        public TValue GetValue(TEntity source)
+        {
+            if (source == null) throw new ArgumentNullException("source");
+
+            return _valueGetter(source);
+        }
+
+        public TEntity SetValue(TEntity target, TValue value)
+        {
+            if (target == null) throw new ArgumentNullException("target");
+
+            if (IsReadOnly)
+                throw new InvalidOperationException(string.Format("Property '{0}' is read only.", PropertyName));
+
+            _valueSetter(target, value);
+
+            return target;
+        }
+
+        private static string GetPropertyName(Expression<Func<TEntity, TValue>> expression)
+        {
+            var body = expression.Body as MemberExpression;
+
+            if (body == null)
+            {
+                throw new ArgumentException("'expression' should be a property expression");
+            }
+
+            if (!(body.Expression is ParameterExpression))
+            {
+                const string message = "'expression' should be direct, properties nested to other objects are not supported (i.e. lambdas like v => vm.Object.Property)";
+                throw new ArgumentException(message);
+            }
+
+            return body.Member.Name;
+        }
+    }
+}
