9b0708d8868436f7b6badb5e8e5ef8a0343df9ba
octahedrone <octahedrone@gmail.com>
2015-04-25 22:49:19 +0300

    Implementing IPropertyAdapter by ValueTypeGenericPropertyAdapter.

---
+++ src/Composite.Core.Tests/ValueTypeGenericPropertyAdapterTests.cs
@@ -0,0 +1,55 @@
+﻿using Composite.Core.PropertyAdapaters;
+using Composite.Core.Tests.StructTargetTests.Target;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace Composite.Core.Tests
+{
+    [TestFixture]
+    public class ValueTypeGenericPropertyAdapterTests
+    {
+        [Test]
+        public void PropertyNameIsRetrievedCorrectly()
+        {
+            var sut = new ValueTypeGenericPropertyAdapter<EditableStruct, string>(_ => _.Text);
+
+            // assert
+            sut.PropertyName.Should().Be("Text");
+            sut.IsReadOnly.Should().BeTrue();
+        }
+
+        [Test]
+        public void PropertyValueIsRetrievedCorrectly()
+        {
+            var sut = new ValueTypeGenericPropertyAdapter<EditableStruct, string>(_ => _.Text);
+
+            // act
+            const string text = "Value";
+            var value = sut.GetValue(new EditableStruct { Text = text });
+
+            // assert
+            value.Should().Be(text);
+        }
+
+        [Test]
+        public void PropertyValueIsUpdatedCorrectly()
+        {
+            var sut = new ValueTypeGenericPropertyAdapter<EditableStruct, string>(_ => _.Text, (t, v) =>
+            {
+                t.Text = v;
+                return t;
+            });
+
+            const string initialText = "Initial";
+            const string updatedText = "Updated";
+
+            // act
+            var target = new EditableStruct { Text = initialText };
+            target = sut.SetValue(target, updatedText);
+
+            // assert
+            sut.IsReadOnly.Should().BeFalse();
+            target.Text.Should().Be(updatedText);
+        }
+    }
+}
---
+++ src/Composite.Core/PropertyAdapaters/ValueTypeGenericPropertyAdapter.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace Composite.Core.PropertyAdapaters
+{
+    public class ValueTypeGenericPropertyAdapter<TEntity, TValue> : IPropertyAdapter<TEntity, TValue>
+        where TEntity : struct
+    {
+        private readonly string _propertyName;
+        private readonly Func<TEntity, TValue, TEntity> _valueSetter;
+        private readonly Func<TEntity, TValue> _valueGetter;
+
+        public ValueTypeGenericPropertyAdapter(Expression<Func<TEntity, TValue>> getterExpression)
+            : this(getterExpression, null)
+        {
+        }
+
+        public ValueTypeGenericPropertyAdapter(Expression<Func<TEntity, TValue>> getterExpression,
+            Func<TEntity, TValue, TEntity> valueSetter)
+        {
+            _valueGetter = getterExpression.Compile();
+            _valueSetter = valueSetter;
+
+            _propertyName = getterExpression.Body.GetPropertyName();
+        }
+
+        public string PropertyName
+        {
+            get { return _propertyName; }
+        }
+
+        public bool IsReadOnly
+        {
+            get { return _valueSetter == null; }
+        }
+
+        public TValue GetValue(TEntity source)
+        {
+            return _valueGetter(source);
+        }
+
+        public TEntity SetValue(TEntity target, TValue value)
+        {
+            if (IsReadOnly)
+                throw new InvalidOperationException(string.Format("Property '{0}' is read only.", PropertyName));
+
+            return _valueSetter(target, value);
+        }
+    }
+}
