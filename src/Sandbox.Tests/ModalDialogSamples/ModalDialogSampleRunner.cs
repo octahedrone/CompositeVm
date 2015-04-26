0b7fb5b3fa38a0d2803f4e6a07d4bafe99ba8a8b
octahedrone <octahedrone@gmail.com>
2015-04-26 12:27:42 +0300

    Manual window launch from unit test method

---
+++ src/Sandbox.Tests/ModalDialogSamples/ModalDialogSampleRunner.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows.Controls;
+using NUnit.Framework;
+
+namespace Sandbox.Tests.ModalDialogSamples
+{
+    [TestFixture]
+    [RequiresSTA]
+    [Ignore("UI blocking test")]
+    public class ModalDialogSampleRunner
+    {
+        [Test]
+        public void RunSampleEditor()
+        {
+            var testData = new TextBlock {Text = "Wa-ha!"};
+
+            testData.ShowModal();
+        }
+    }
+}
---
+++ src/Sandbox.Tests/TestWindowLauncher.cs
@@ -0,0 +1,50 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+
+namespace Sandbox.Tests
+{
+    public static class TestWindowLauncher
+    {
+        public static void ShowModal(this object dataContext)
+        {
+            ShowModal(dataContext, null);
+        }
+
+        public static void ShowModal(this object dataContext, ResourceDictionary dictionary)
+        {
+            var window = new Window { Content = dataContext };
+
+            if (dictionary != null)
+                window.Resources.MergedDictionaries.Add(dictionary);
+
+            window.InvalidateVisual();
+
+            window.ShowDialog();
+            window.Dispatcher.InvokeShutdown();
+        }
+
+        public static void ShowModal(this Control content)
+        {
+            ShowModal(content, null);
+        }
+
+        public static void ShowModal(this Control content, ResourceDictionary dictionary)
+        {
+            var window = content as Window;
+
+            if (window == null)
+            {
+                window = new Window {Content = content};
+            }
+
+            if (dictionary != null)
+                window.Resources.MergedDictionaries.Add(dictionary);
+
+            window.InvalidateVisual();
+
+            window.ShowDialog();
+            window.Dispatcher.InvokeShutdown();
+        }
+    }
+}
