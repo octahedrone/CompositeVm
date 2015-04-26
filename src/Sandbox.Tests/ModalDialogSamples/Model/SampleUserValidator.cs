6d6db44556e56b180988d596e222d4f487fe4518
octahedrone <octahedrone@gmail.com>
2015-04-26 14:58:20 +0300

    Adding Name field validation for a sample user editor

--- src/Sandbox.Tests/ModalDialogSamples/Model/SampleUserValidator.cs
+++ src/Sandbox.Tests/ModalDialogSamples/Model/SampleUserValidator.cs
@@ -1,4 +1,6 @@
-﻿using Composite.Core.Validation;
+﻿using System;
+using System.Collections.Generic;
+using Composite.Core.Validation;
 using Sandbox.Tests.Validation;
 
 namespace Sandbox.Tests.ModalDialogSamples.Model
@@ -7,6 +9,14 @@ namespace Sandbox.Tests.ModalDialogSamples.Model
     {
         public ValidationState Validate(SampleUser target)
         {
+            if (String.IsNullOrEmpty(target.Name) || string.IsNullOrWhiteSpace(target.Name))
+            {
+                return new ValidationState(new Dictionary<string, string>
+                {
+                    {"Name", "Please enter non-empty or whitespace only name"}
+                });
+            }
+
             return ValidationState.Valid;
         }
     }
